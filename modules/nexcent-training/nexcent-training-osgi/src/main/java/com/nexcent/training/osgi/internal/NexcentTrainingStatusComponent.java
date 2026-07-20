851eed540f0629417907321abadbf459ae579262
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-07-20 14:36:20 +0700

    feat(training): add configurable OSGi status component

---
+++ modules/nexcent-training/nexcent-training-osgi/src/main/java/com/nexcent/training/osgi/internal/NexcentTrainingStatusComponent.java
@@ -0,0 +1,49 @@
+package com.nexcent.training.osgi.internal;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.nexcent.training.osgi.configuration.NexcentTrainingConfiguration;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.Designate;
+
+@Component(
+    configurationPid = "com.nexcent.training.osgi.configuration.NexcentTrainingConfiguration",
+    immediate = true,
+    service = NexcentTrainingStatusComponent.class
+)
+@Designate(ocd = NexcentTrainingConfiguration.class)
+public class NexcentTrainingStatusComponent {
+
+    public String getSummary() {
+        return String.format(
+            "%s is active. Portal user count: %d",
+            _siteLabel, _userLocalService.getUsersCount());
+    }
+
+    @Activate
+    @Modified
+    protected void activate(Map<String, Object> properties) {
+        _siteLabel = GetterUtil.getString(
+            properties.get("siteLabel"), "Nexcent Training Site");
+
+        if (_log.isInfoEnabled()) {
+            _log.info(getSummary());
+        }
+    }
+
+    private static final Log _log = LogFactoryUtil.getLog(
+        NexcentTrainingStatusComponent.class);
+
+    private volatile String _siteLabel;
+
+    @Reference
+    private UserLocalService _userLocalService;
+}

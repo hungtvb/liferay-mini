0031230480633cec486ce13b7e96e584e981ac55
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-07-26 17:33:51 -0700

    refactor(frontend): compose preview from extracted production sections

--- client-extensions/nexcent-landing-elements/src/static-site/StaticPage.tsx
+++ client-extensions/nexcent-landing-elements/src/static-site/StaticPage.tsx
@@ -1,15 +1,13 @@
-import {
-    StaticClients,
-    StaticCommunity,
-    StaticCta,
-    StaticFeature,
-    StaticMarketing,
-    StaticStatistics,
-    StaticTestimonial,
-} from './components/ContentSections';
+import {Articles} from './components/Articles/Articles';
+import {Clients} from './components/Clients/Clients';
+import {Community} from './components/Community/Community';
+import {Cta} from './components/Cta/Cta';
+import {Feature} from './components/Feature/Feature';
 import {StaticFooter} from './components/Footer';
 import {StaticHeader} from './components/Header';
 import {StaticHero} from './components/Hero';
+import {Statistics} from './components/Statistics/Statistics';
+import {Testimonial} from './components/Testimonial/Testimonial';
 
 export function StaticPage() {
     return (
@@ -17,14 +15,14 @@ export function StaticPage() {
             <StaticHeader />
             <main className="page">
                 <StaticHero />
-                <StaticClients />
-                <StaticCommunity />
-                <StaticFeature featureKey="primary" />
-                <StaticStatistics />
-                <StaticFeature featureKey="secondary" />
-                <StaticTestimonial />
-                <StaticMarketing />
-                <StaticCta />
+                <Clients />
+                <Community />
+                <Feature featureKey="primary" />
+                <Statistics />
+                <Feature featureKey="secondary" />
+                <Testimonial />
+                <Articles />
+                <Cta />
             </main>
             <StaticFooter />
         </div>
